d8746878a21367ca91d565129eb08740d9e06837
Ibram Marzouk <abram.atef@gmail.com>
2016-06-04 21:13:30 -0400

    Checking if the wordlist file exists or not
    Before this, if the wordlist doesn't exist.. the program would throw this ugly error: `IOError: [Errno 2] No such file or directory: 'wordlist.txt'`!
    But now.. It would nicely say that the file doesn't exist. :)

--- BasicAuth-Brute.py
+++ BasicAuth-Brute.py
@@ -5,6 +5,7 @@
 # Purpose: ducational purpose (Cracking Basic Authentication)
 
 from sys import argv
+import os.path
 import requests
 import base64
 from colorama import Fore as foreground
@@ -12,8 +13,12 @@ from colorama import Fore as foreground
 if len(argv) != 3:
 	print "Usage:\n\t python %s <wordlist> <url>\n"%argv[0]
 	exit(1)
+if(os.path.isfile(argv[1])):
+	wordlist = open(argv[1], "r")
+else:
+	print "Error:\n\t Wordlist not found!\n"
+	exit(1)
 
-wordlist = open(argv[1], "r")
 # Login Page URL
 url = argv[2]
 words = wordlist.readlines()
